73e399fae9cab4aaad17e2e05d15ddcf6cd40a30
Olivia <olivia.bsmith94@gmail.com>
2022-08-18 08:58:04 +0100

    Adding function to check products

--- OLIVIAS_VENDING_APP/main.py
+++ OLIVIAS_VENDING_APP/main.py
@@ -46,7 +46,7 @@ class MainWindow:
         self.money_buttons()
         
 
-        self.ui.buybtn.clicked.connect(self.buy_vending)
+        #self.ui.buybtn.clicked.connect(self.buy_vending)
 
     def vending_buttons(self):
         #Set event when button clicked
@@ -162,22 +162,22 @@ class MainWindow:
         self.enable_buttons(self.snackdict,True)
 
 
-    def buy_vending(self):
-        self.ui.buybtn.setEnabled(False)
-        self.change = VendingApp.change_vending(self)
-        self.change = round(self.change,2)
-        print(self.change)
-        if self.change >= 0:
-            self.change_coins = VendingApp.check_machine_coins(self)
-            self.ui.stackedWidget.setCurrentWidget(self.ui.change)
-            VendingApp.remove_product(self,self.buy_product)
-            self.add_items()
+    #def buy_vending(self):
+    #    self.ui.buybtn.setEnabled(False)
+    #    self.change = VendingApp.change_vending(self)
+    #    self.change = round(self.change,2)
+    #    print(self.change)
+    #    if self.change >= 0:
+    #        self.change_coins = VendingApp.check_machine_coins(self)
+    #        self.ui.stackedWidget.setCurrentWidget(self.ui.change)
+    #        VendingApp.remove_product(self,self.buy_product)
+    #        self.add_items()
 
-    def add_items(self):
-        list_ch = self.change_coins
-        for i in range(len(list_ch)):
-            item = QTableWidgetItem(str(list_ch[i]))
-            self.ui.change_table.setItem((i), 1, item)
+    #def add_items(self):
+    #    list_ch = self.change_coins
+    #    for i in range(len(list_ch)):
+    #        item = QTableWidgetItem(str(list_ch[i]))
+    #        self.ui.change_table.setItem((i), 1, item)
 
 
 if __name__ == '__main__':
